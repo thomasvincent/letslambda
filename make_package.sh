fc0ae27a643182b9269ac659295654c99198288c
Aurélie Requiem <aurelien@requiem.fr>
2017-04-16 01:13:50 +0000

    cleanup in .gitignore and adjustements in make_package.sh

--- make_package.sh
+++ make_package.sh
@@ -1,15 +1,20 @@
 #!/bin/sh -e
 
-TARGET="tmp"
+TARGET="build"
+DIST="dist"
+DEST="$DIST/letslambda.zip"
 
 test -d .env || (echo "missing virtualenv environement."; exit 1)
 
 test ! -d "$TARGET" || rm -rf $TARGET
+test ! -d "$DIST" || rm -rf $DIST
 mkdir $TARGET
+mkdir $DIST
 
 cp -pdr .env/lib/python2.7/site-packages/* "$TARGET/"
 cp -pdr .env/lib64/python2.7/site-packages/* "$TARGET/"
 cp -pdr .env/src/acme/acme/acme* "$TARGET/"
 cp -pd letslambda.py "$TARGET/"
 
-cd "$TARGET" && zip --recurse-paths -9 ../letslambda.zip ./
+rm -f "$DEST"
+cd "$TARGET" && zip --recurse-paths -9 "../$DEST" ./
