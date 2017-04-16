f9d8e4147b8942d6087038166b51d1df7db2b10f
Aurélie Requiem <aurelien@requiem.fr>
2017-04-16 23:51:07 +0000

    improve the way the lambda function is packaged

--- make_package.sh
+++ make_package.sh
@@ -1,5 +1,16 @@
 #!/bin/sh -e
 
+. /etc/os-release
+case "$ID" in
+	"amzn")
+		echo "Packaging..."
+	;;
+	"*")
+		echo "An AWS Lambda function should only be packaged from an Amazon Linux to prevent broken shared library dependencies"
+		exit
+	;;
+esac
+
 TARGET="build"
 DIST="dist"
 DEST="$DIST/letslambda.zip"
@@ -8,12 +19,12 @@ test -d .env || (echo "missing virtualenv environement."; exit 1)
 
 test ! -d "$TARGET" || rm -rf $TARGET
 test ! -d "$DIST" || rm -rf $DIST
-mkdir $TARGET
-mkdir $DIST
+mkdir $TARGET $DIST
 
-cp -pdr .env/lib/python2.7/site-packages/* "$TARGET/"
-cp -pdr .env/lib64/python2.7/site-packages/* "$TARGET/"
-cp -pdr .env/src/acme/acme/acme* "$TARGET/"
+cp -pdr \
+	$(find .env/lib64/python2.7/site-packages/ -maxdepth 1 -not -path .env/lib64/python2.7/site-packages/) \
+	$(find .env/lib/python2.7/site-packages/ -maxdepth 1 -not -path .env/lib/python2.7/site-packages/) \
+	"$TARGET/"
 cp -pd letslambda.py "$TARGET/"
 
 rm -f "$DEST"
